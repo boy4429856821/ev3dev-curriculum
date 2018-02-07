3787ef612ebd2c1ec3174ff463ea0245841b88f2
hartmagj <hartmagj@rose-hulman.edu>
2018-02-07 17:04:13 -0500

    fixed m1 in digital inputs

--- sandbox/hartmagj/analog_sensors/ir_sensor/m1_beep_at_hands.py
+++ sandbox/hartmagj/analog_sensors/ir_sensor/m1_beep_at_hands.py
@@ -8,8 +8,8 @@ Check every 0.1 seconds for a hand and print the proximity value each check.
 
 Setup the program to continue to run until the user presses the touch sensor.
 
-Authors: David Fisher and PUT_YOUR_NAME_HERE.  February 2017.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: David Fisher and Grant Hartman.  February 2017.
+"""  # d: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import ev3dev.ev3 as ev3
 import time
@@ -29,13 +29,17 @@ def main():
     # Presumably you added this in the digital_inputs unit, if not add it now so that
     # the code below works to monitor the touch_sensor.
 
-    while not robot.touch_sensor.is_pressed:
+    while not robot.TouchSensor.is_pressed:
         # TODO: 2. Implement the module as described in the opening comment block.
         # It is recommended that you add to your Snatch3r class's constructor the ir_sensor, as shown
         #   self.ir_sensor = ev3.InfraredSensor()
         #   assert self.ir_sensor
         # Then here you can use a command like robot.ir_sensor.proximity
+        if robot.ir_sensor.proximity < 10:
+            ev3.Sound.beep()
+            time.sleep(1.5)
 
+        print(robot.ir_sensor.proximity)
         time.sleep(0.1)
 
     # TODO: 3. Call over a TA or instructor to sign your team's checkoff sheet.
