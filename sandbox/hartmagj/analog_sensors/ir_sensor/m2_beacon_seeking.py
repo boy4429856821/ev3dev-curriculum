4714cdd015cc7e3cacfcce9944f3abb56b79621c
hartmagj <hartmagj@rose-hulman.edu>
2018-02-11 14:12:56 -0500

    finished beacon seeking thing

--- sandbox/hartmagj/analog_sensors/ir_sensor/m2_beacon_seeking.py
+++ sandbox/hartmagj/analog_sensors/ir_sensor/m2_beacon_seeking.py
@@ -29,11 +29,12 @@ def main():
     robot = robo.Snatch3r()
     try:
         while True:
-            seek_beacon(robot)
+            found = seek_beacon(robot)
 
-            # TODO: 5. Save the result of the seek_beacon function (a bool), then use that value to only say "Found the
+            # d: 5. Save the result of the seek_beacon function (a bool), then use that value to only say "Found the
             # beacon" if the return value is True.  (i.e. don't say "Found the beacon" if the attempts was cancelled.)
-            ev3.Sound.speak("Found the beacon")
+            if found:
+                ev3.Sound.speak("Found the beacon")
 
             command = input("Hit enter to seek the beacon again or enter q to quit: ")
             if command == "q":
@@ -75,7 +76,7 @@ def seek_beacon(robot):
             print("IR Remote not found. Distance is -128")
             robot.shutdown()
         else:
-            # TODO: 4. Implement the following strategy to find the beacon.
+            # d: 4. Implement the following strategy to find the beacon.
             # If the absolute value of the current_heading is less than 2, you are on the right heading.
             #     If the current_distance is 0 return from this function, you have found the beacon!  return True
             #     If the current_distance is greater than 0 drive straight forward (forward_speed, forward_speed)
